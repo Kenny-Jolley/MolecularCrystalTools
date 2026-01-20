5a90b92ff4e3858605eced4426c0116684b1de25
Kenny Jolley <kenny.jolley@gmail.com>
2026-01-20 17:24:38 +0000

    Update overlay_hnb_2_molecules.sh
    correct comment to 2 molecules in this case.

--- example_overlay/overlay_hnb_2_molecules.sh
+++ example_overlay/overlay_hnb_2_molecules.sh
@@ -4,5 +4,5 @@
 echo Unwrapping hnb.xyz first
 python ../scripts/unwrap_cell.py hnb.xyz hnb_unwrapped.xyz
 
-# Overlay the 1 molecule structure on top
+# Overlay the 2 molecule structure on top
 python ../scripts/overlay_molecules.py hnb_unwrapped.xyz hnb_2_molecules.xyz --rot_steps=3
